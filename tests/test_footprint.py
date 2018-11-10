66875928cf531efe39ba6b684f2a1c32c62e6b5e
iwbailey <iainbailey@gmail.com>
2018-11-10 21:45:29 +0000

    Additional tests for when we're considering std deviation in the footprint

--- tests/test_footprint.py
+++ tests/test_footprint.py
@@ -17,7 +17,7 @@ from shakemap_oasisloss import BinIntervals
 ifile = os.path.join(os.path.expanduser('~'), 'Downloads', 'usgs_shakemap',
                      'grid_70116556_v01.0.xml.zip')
 ifile_unc = os.path.join(os.path.expanduser('~'), 'Downloads', 'usgs_shakemap',
-                     'uncertainty_70116556_v01.0.xml.zip')
+                         'uncertainty_70116556_v01.0.xml.zip')
 
 # which intensity measure to use
 intensMeasure = 'MMI'
@@ -33,7 +33,8 @@ eventId = 1
 # Script -----
 
 # load the shakemap into a class
-sm = USGSshakemapGrid(ifile, intensMeasure)
+#sm = USGSshakemapGrid(ifile, intensMeasure)
+sm = USGSshakemapGrid(ifile, intensMeasure, ifile_unc)
 
 # Define an area peril grid from the shakemap properties
 geoGrid = AreaPerilGrid(sm.xlims(False), sm.nx(), sm.ylims(False), sm.ny())
@@ -41,15 +42,18 @@ geoGrid = AreaPerilGrid(sm.xlims(False), sm.nx(), sm.ylims(False), sm.ny())
 # Define a footprint
 fp = ShakemapFootprint(eventId, sm, geoGrid, binedges[0])
 
-# display to terminal
-print('\nFootprint table:')
-print(fp.df.head(10))
-
 # Set up the intensity bins, display
 intensbins = BinIntervals(binedges, closed='left')
+
+# display to terminal
+print('\nFootprint table:')
+print(fp.df.head(20))
+print('...')
+
 print('\nIntensity bins:')
-print(intensbins.df)
+print(intensbins.df.head(10))
+print('...')
 
 # Get the table in oasis format with no std deviation
 print("\nFootprint:")
-print(fp.as_oasistable(intensbins).head(10))
+print(fp.as_oasistable(intensbins).head(20))
