3713441b2e38d20651985a5563c5db0edfe541e7
iwbailey <iainbailey@gmail.com>
2018-11-22 22:02:16 +0000

    Initialization now uses intervalindex rather than array breaks. Damage function stored now as a pandas dataframe isntead of numpy array. Keep store of the fragility functions with original damage states for reference.

--- shakemap_oasisloss/damagefunction.py
+++ shakemap_oasisloss/damagefunction.py
@@ -6,63 +6,81 @@
 import numpy as np
 import pandas as pd
 from scipy import interpolate
+import pdb
 
 
 class DamageFunction:
     """Class for an oasis style damage function """
-    def __init__(self, vulnId, frag, damageStateDict, intensBinCentres,
-                 drBinEdges):
+    def __init__(self, vulnId, frag, damageStateDict, intensBins,
+                 drBins):
         """ Constructor """
 
         # Assign the vuln id
         self.vulnId = vulnId
 
-        # Interpolate the fragility curve exceedance probabilities to the
-        # bottom of the bins
-        df = pd.DataFrame(columns=frag.damagestates())
+        # Interpolate the fragility curve of each damage state to get
+        # exceedance probabilities at each intensity
+        exprob_df = pd.DataFrame(columns=frag.damagestates(), index=intensBins)
         for c in frag.damagestates():
-            df[c] = frag.interp_damagestate(intensBinCentres, c)
+            exprob_df[c] = frag.interp_damagestate(intensBins.mid, c)
 
-        # Assign the intepolated results to the damage ratios
+        # Save the dataframe as a property of the class
+        self.exprobcurves = exprob_df
+
+        # Assign the damage state names to  damage ratios
         drVals = [damageStateDict[x] for x in frag.damagestates()]
 
-        # Interpolation function for current damage states to higher resolution
+        # Interpolation function for current damage ratios to higher resolution
         def myinterp(x):
-            return interpolate.pchip_interpolate(drVals, x, drBinEdges[:-1])
+            return interpolate.pchip_interpolate(drVals, x, drBins.left.values)
 
         # Run the interpolation for each row, i.e. each intensity bin, output
-        # as array with intensity on rows, damage along columns
-        probEx = (np.apply_along_axis(myinterp, axis=1, arr=df.values)).T
+        # the prob of exceeding the damage ratio
+        probEx = np.apply_along_axis(myinterp, axis=1, arr=exprob_df.values)
 
-        # Store the discrete results
-        self.prob = np.vstack((np.diff(-probEx, axis=0), probEx[-1, :]))
+        # Convert to probability of within this damage ratio. The final bin
+        # remains the same.
+        prob = probEx
+        prob[:, :-1] = np.diff(-probEx, axis=1)
+
+        # store as a data frame, with intensities as rows, damage as columns
+        self.prob = pd.DataFrame(prob, index=intensBins, columns=drBins)
 
         # TODO deal with when the minimum
 
         return
 
-    def vulnarr_to_oasis(self, minProb=1e-6):
+    def vulnarr_to_oasis(self, intensbins, drbins, minProb=1e-6):
         """Convert from a vulnerability matrix with damage states as different rows,
         intensity bins as different columns, each column providing the discrete
         prob distribution
 
         """
 
-        nrow = self.prob.shape[0]
-        ncol = self.prob.shape[1]
+        # Convert to damage function from 2-d array to  single column
+        p = self.prob.stack().to_frame(name='prob')
+        p.index.set_names(['intens_bin', 'dmg_bin'], inplace=True)
 
-        damBinIdx = np.arange(1, nrow + 1)
-        intensBinIdx = np.arange(1, ncol + 1)
+        # Drop the low probability ones
+        p = p[p.prob >= minProb]
 
-        oasisVuln = pd.DataFrame.from_dict({'vulnerability_id':
-                                            np.repeat(self.vulnId, nrow*ncol),
-                                            'intensity_bin_index':
-                                            np.tile(intensBinIdx, nrow),
-                                            'damage_bin_index':
-                                            np.repeat(damBinIdx, ncol),
-                                            'prob':
-                                            self.prob.flatten()})
+        # Join with the intensity bins
+        intensbins = intensbins.bin_id.to_frame(name='intensity_bin_id')
+        intensbins.index.name = 'intens_bin'
+        p = p.join(intensbins, how='left')
 
-        # Get rid of zeros
+        # Join with the damage bins
+        drbins = drbins.bin_id.to_frame(name='damage_bin_id')
+        drbins.index.name = 'dmg_bin'
+        p = p.join(drbins, how='left')
 
-        return oasisVuln[oasisVuln.prob >= minProb]
+        # Drop the multi-index
+        p = p.reset_index(drop=True)
+
+        # Assign the vulnerability id
+        p = p.assign(vulnerability_id=self.vulnId)
+
+        # Return the necessary columns in the right order
+        # TODO: check order is correct
+        return p.loc[:, ['vulnerability_id', 'intensity_bin_id',
+                         'damage_bin_id', 'prob']]
--- tests/test_damagefunction.py
+++ tests/test_damagefunction.py
@@ -1,12 +1,16 @@
+# Test that the damage function is working
 
-
-import numpy as np
 import os
+import sys
+import numpy as np
+import pandas as pd
+
 from matplotlib import pyplot as plt
 from matplotlib import pylab
 
 from shakemap_lookup import FragilityCurve
 from shakemap_oasisloss import DamageFunction
+from shakemap_oasisloss import BinIntervals
 
 
 # Parameters ----
@@ -23,43 +27,117 @@ damageStateDict = {
     'destruction': 1.0}
 
 # Intensity bin edges
-intensbins = np.arange(5.45, 10.15, 0.1)
+intensbinedges = np.linspace(5.0, 11.0, 61)
 
 # damage ratio bin edges
-drbins = np.arange(0.0, 1.01, 0.01)
+drbinedges = np.linspace(0.0, 1.0, 101)
 
 # Vulnerabilty id
 vulnId = 1
+
+# Limit pandas display size
+pd.options.display.max_rows = 10
+
+# Functions -----
+
+
+def checkplot_interpolatedamage(frag, exprobcurves):
+    """Read in fragility function and plot
+
+    Plot the fragility curves as read in
+    IN: Pandas data frame
+          column 1 is named 'MMI'
+          columns 2+ are named the damage states
+    """
+
+    fig, ax = plt.subplots(1, 1, facecolor='white')
+
+    # Plot underlying fragility curve
+    for c in frag.damagestates():
+        ax.plot(frag.intensities, frag.exceedprob[c].values, label=c)
+        ax.plot(exprobcurves.index.mid, exprobcurves[c].values, '--', label=c)
+    ax.set_xlabel(frag.intensitymeasure)
+    ax.set_ylabel('ProbExceedance')
+    ax.grid(True)
+    ax.legend(loc=2, frameon=False, framealpha=0.5)
+
+    plt.title('Fragility curve')
+
+    return
+
 
 # Script -----
 
 # Read in fragility curve
 frag = FragilityCurve(ifile)
 
+# Set up the damage bins
+drbins = BinIntervals(drbinedges, closed="right")
+print('\nDamage ratio bins:')
+print(drbins.bin_id)
+
+# Set up the intensity bins, display
+intensbins = BinIntervals(intensbinedges, closed='left')
+print('\nIntensity bins:')
+print(intensbins.bin_id)
+
 # Set up the damage function
 dmg = DamageFunction(vulnId, frag, damageStateDict,
-                     0.5*(intensbins[:-1] + intensbins[1:]), drbins)
+                     intensbins.intervals, drbins.intervals)
 
-# Plot
+print('\nDamage prob matrix as data frame:')
+print(dmg.prob)
+
+# Test the indexing
+testIntens = 5.278
+print('\nDamage Ratio PDF for intensity=%.2f:' % testIntens)
+print(dmg.prob.loc[testIntens])
+print("Sum probs = %.2f" % dmg.prob.loc[testIntens].sum())
+
+testDRbin = 2
+print('\nProbability of damage in interval %s for intensity=%.2f:' %
+      (dmg.prob.columns[testDRbin], testIntens))
+print(dmg.prob.loc[testIntens].iloc[testDRbin])
+
+# Convert to pandas array with oasis headers
+oasisVuln = dmg.vulnarr_to_oasis(intensbins, drbins, minProb=1e-9)
+
+# display to terminal
+print('Footprint table:')
+print(oasisVuln)
+
+# Plot the input fragility function vs interpolated
+checkplot_interpolatedamage(frag, dmg.exprobcurves)
+
+# Plot as an array
 fig, ax = plt.subplots(1, 1, facecolor='white')
-cax = ax.imshow(dmg.prob, interpolation='none', origin='lower',
+cax = ax.imshow(dmg.prob.values.T, interpolation='none', origin='lower',
                 aspect='auto',
-                extent=(intensbins[0], intensbins[-1],
-                        drbins[0], drbins[-1]))
+                extent=(intensbinedges[0], intensbinedges[-1],
+                        drbinedges[0], drbinedges[-1]))
 plt.xlabel(frag.intensitymeasure)
 plt.ylabel('Damage Ratio')
 ax.grid()
 plt.colorbar(cax, ax=ax, orientation='vertical',
              label="Probability")
 
+# Plot exceedance prob as an array
+exprob = np.cumsum(dmg.prob.values.T[::-1, :], axis=0)[::-1, :]
+fig, ax = plt.subplots(1, 1, facecolor='white')
+cax = ax.imshow(exprob, interpolation='none', origin='lower',
+                aspect='auto',
+                extent=(intensbinedges[0], intensbinedges[-1],
+                        drbinedges[0], drbinedges[-1]),
+                vmin=0, vmax=1)
+plt.xlabel(frag.intensitymeasure)
+plt.ylabel('Damage Ratio')
+ax.grid()
+plt.colorbar(cax, ax=ax, orientation='vertical',
+             label="Exceedance Probability")
+
+
 print('Pausing while plot is shown...')
 pylab.show(block=True)
 
-# Convert to pandas array with oasis headers
-oasisVuln = dmg.vulnarr_to_oasis()
 
-# display to terminal
-print('Footprint table:')
-print(oasisVuln.head(10))
-print("...")
-print(oasisVuln.tail(10))
+sys.exit()
