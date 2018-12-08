d3a3e0a2fbf3cafa1f534d261769adaffee873e2
iwbailey <iainbailey@gmail.com>
2018-12-08 20:50:31 +0000

    Example loss calculation workflow and many results check plots. Now working.

--- example/create_nonstatic_inputs.py
+++ example/create_nonstatic_inputs.py
@@ -9,19 +9,24 @@
 
 import yaml
 import sys
-import numpy as np
 import pandas as pd
 from csv import QUOTE_NONNUMERIC as CSVQUOTE
 
 from shakemap_oasisloss import AreaPerilGrid
 
 # Parameters ------------------------------------------------------------------
-ifile_loc = "./inputs/Hawaii_Mile_Markers_v2.csv"
+ifile_loc = "./example_data/Hawaii_Mile_Markers_v2.csv"
+# ifile_loc = "./example_data/Hawaii_Mile_Markers_top15.csv"
 ifile_areaperil = "./areaperilgrid.yaml"
 vulnId = 1
 
 ofile_items = 'input/items.csv'
 ofile_cvg = 'input/coverages.csv'
+ofile_gulsumm = 'input/gulsummaryxref.csv'
+ofile_events = 'input/events.csv'
+
+ofile_locns = 'input/locations_encoded.csv'
+
 
 # Functions -------------------------------------------------------------------
 
@@ -33,7 +38,7 @@ def locns2cvgs(locns):
 
     """
     coverages = pd.DataFrame.from_dict({
-        'coverage_id': locns['id'].values,
+        'coverage_id': locns['coverage_id'].values,
         'tiv': locns['tiv'].values
     })
     return coverages
@@ -45,15 +50,33 @@ def locns2items(locns):
     item_id and coverage_id are the same
 
     """
-    items = pd.DataFrame.from_items((
-        ('item_id', locns['id'].values),
-        ('coverage_id', locns['id'].values),
-        ('areaperil_id', locns['areaperil_id']),
-        ('vulnerability_id', locns['vulnerability_id']),
-        ('group_id', locns['id'].values)  # this defines 100% correlation
-    ))
+    items = pd.DataFrame.from_dict({
+        'item_id': locns['coverage_id'].values,
+        'coverage_id': locns['coverage_id'].values,
+        'areaperil_id': locns['areaperil_id'].values,
+        'vulnerability_id': locns['vunerability_id'].values,
+        'group_id': locns['id'].values  # this defines 100% correlation
+    })
 
     return items
+
+
+def locns2gulsummary(locns):
+    """Get one summary result per location
+
+    summaryset_id is the reporting level
+    """
+    gulsummary1 = pd.DataFrame.from_dict({
+        'coverage_id': locns['coverage_id'].values,
+        'summary_id': locns['coverage_id'].values
+    }).assign(summaryset_id=1)
+
+    gulsummary2 = pd.DataFrame.from_dict({
+        'coverage_id': locns['coverage_id'].values,
+        'summary_id': 1,
+    }).assign(summaryset_id=2)
+
+    return pd.concat([gulsummary1, gulsummary2], ignore_index=False)
 
 
 # Script ----------------------------------------------------------------------
@@ -83,10 +106,15 @@ areaperilids = areaperilMap.assign_xytoid(locns.lon, locns.lat)
 locns = locns.assign(areaperil_id=areaperilids.data)
 
 # Remove locations that were not within grid
-print("Removing %i locations not geoencoded to the grid space" %
-      sum(1-areaperilids.mask))
-locns = locns[areaperilids.mask == 1]
-print("%i locations left" % len(locns))
+if areaperilids.mask.any():
+    print("Removing %i locations not geoencoded to the grid space" %
+          sum(1-areaperilids.mask))
+    locns = locns[areaperilids.mask == 0]
+    print("%i locations left" % len(locns))
+
+# Coverage and item IDs need to start at 1
+locns.reset_index(drop=True, inplace=True)
+locns = locns.assign(coverage_id=locns.index+1)
 
 # Assign all the same vulnerability id
 locns['vunerability_id'] = 1
@@ -99,6 +127,20 @@ coverages.to_csv(ofile_cvg, index=False, quoting=CSVQUOTE)
 print("Written to %s" % ofile_cvg)
 
 # Write items to files
-items = locns2cvgs(locns)
+items = locns2items(locns)
 items.to_csv(ofile_items, index=False, quoting=CSVQUOTE)
 print("Written to %s" % ofile_items)
+
+# Write location details to flie
+locns.to_csv(ofile_locns, index=False, quoting=CSVQUOTE)
+print("Written to %s" % ofile_locns)
+
+# Write gulsummary to file
+gulsummary = locns2gulsummary(locns)
+gulsummary.to_csv(ofile_gulsumm, index=False, quoting=CSVQUOTE)
+print("Written to %s" % ofile_gulsumm)
+
+# Write list of events to file
+events = pd.DataFrame([1], columns=['event_id'])
+events.to_csv(ofile_events, index=False, quoting=CSVQUOTE)
+print("Written to %s" % ofile_events)
--- example/create_static_inputs.py
+++ example/create_static_inputs.py
@@ -3,6 +3,7 @@
 import os
 from csv import QUOTE_NONNUMERIC as CSVQUOTE
 import numpy as np
+import pandas as pd
 
 from shakemap_lookup import FragilityCurve
 from shakemap_lookup import USGSshakemapGrid
@@ -17,9 +18,14 @@ from shakemap_oasisloss import DamageFunction
 
 ifileSM = os.path.join(os.path.expanduser('~'), 'Downloads', 'usgs_shakemap',
                        'grid_70116556_v01.0.xml.zip')
-ifileSMunc = os.path.join(os.path.expanduser('~'), 'Downloads',
-                          'usgs_shakemap',
-                          'uncertainty_70116556_v01.0.xml.zip')
+
+isUnc = True
+if isUnc:
+    ifileSMunc = os.path.join(os.path.expanduser('~'), 'Downloads',
+                              'usgs_shakemap',
+                              'uncertainty_70116556_v01.0.xml.zip')
+else:
+    ifileSMunc = None
 
 ifileFrag = '/home/iwbailey/projects/shakemap_lookup/inputs/my_fragility.csv'
 
@@ -33,20 +39,23 @@ damageStateDict = {
 
 intensBinEdges = np.arange(5.45, 10.15, 0.1)
 intensBinEdges[-1] = np.inf
+intensBinEdges = np.insert(intensBinEdges, 0, -np.inf)
 
 # Damage ratio bins
 drBinEdges = np.linspace(0.0, 1.0, 101)
-drBinEdges = np.insert(drBinEdges, 0, 0.0)
+drBinEdges = np.insert(drBinEdges, 0, -np.inf)
 
 vulnerability_id = 1
 event_id = 1
 interval_type = 1201  # Oasis code for interval type
+nRand = int(1e5)
 
 # Output files
 ofile_damdict = 'static/damage_bin_dict.csv'
 ofile_intensdict = 'static/intensity_bin_dict.csv'
 ofile_vuln = 'static/vulnerability.csv'
 ofile_fp = 'static/footprint.csv'
+ofile_rand = 'static/random.csv'
 
 # -----------------------------------------------------------------------------
 
@@ -69,7 +78,8 @@ print("Written to %s" % ofile_intensdict)
 
 # Read in the Shakemap ----
 print("Reaing shakemap...")
-myShakemap = USGSshakemapGrid(ifileSM, 'MMI', ifileSMunc, isQuiet=True)
+myShakemap = USGSshakemapGrid(ifileSM, 'MMI', ifileSMunc, isQuiet=True,
+                              ignoreSVEL600=True)
 
 # Set up the areaperil dict ----
 areaperilMap = AreaPerilGrid(myShakemap.xlims(False), myShakemap.nx(),
@@ -101,3 +111,12 @@ oasisVuln = dmg.vulnarr_to_oasis(intensbins,
                                  damagebins).to_csv(ofile_vuln, index=False,
                                                     quoting=CSVQUOTE)
 print("Written to %s" % ofile_vuln)
+
+
+# Write the random numbers to file
+print("Creating random number file...")
+np.random.seed(12345)
+pd.DataFrame(np.random.rand(nRand, 1),
+             columns=['rand']).to_csv(ofile_rand, index=False,
+                                      quoting=CSVQUOTE)
+print("Written to %s" % ofile_rand)
