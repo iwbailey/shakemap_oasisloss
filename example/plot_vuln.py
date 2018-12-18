6f471e6747b53dac583e8dc7ac337360e3cb4ee8
iwbailey <iainbailey@gmail.com>
2018-12-18 22:20:20 +0000

    Added heatmap

--- example/plot_vuln.py
+++ example/plot_vuln.py
@@ -5,6 +5,7 @@ Plot the damage function.
 from matplotlib import pyplot as plt
 from matplotlib import pylab
 import pandas as pd
+import seaborn as sns
 
 from oasis_utils import read_csvtodf
 
@@ -21,32 +22,72 @@ pd.options.display.max_rows = 10
 # Script ----------------------------------------------------------------------
 
 # Read the location losses from the ELT file
-vuln = read_csvtodf(ifile_vuln)
-vuln = vuln.loc[vuln.vulnerability_id == vulnId]
-vuln.drop('vulnerability_id', 1, inplace=True)
+vuln0 = read_csvtodf(ifile_vuln)
+
+# extract the vulnerability we're interested in
+vuln0 = vuln0.loc[vuln0.vulnerability_id == vulnId]
+vuln0.drop('vulnerability_id', 1, inplace=True)
+
+# Set the index
+vuln0 = vuln0.set_index(['intensity_bin_id', 'damage_bin_id'])
+vuln0 = vuln0.sort_values(['intensity_bin_id', 'damage_bin_id'], axis=0,
+                          ascending=[True, True])
+
+# Calculate cumulative and exceedance prob
+vuln1 = vuln0.groupby('intensity_bin_id').cumsum().rename(columns={'prob':
+                                                                   'cprob'})
+
+# prob of bin and exceding it, not prob of exceeding it
+vuln2 = vuln0.iloc[::-1].groupby('intensity_bin_id').cumsum().rename(columns={
+    'prob': 'exprob'})
+
+# Combine with probabilities
+vuln = pd.concat([vuln0, vuln1, vuln2], axis=1)
 
 # Read the damage bins
 dmg = read_csvtodf(ifile_dmgmap)
 dmg.set_index('index', inplace=True)
+dmg.index.names = ['damage_bin_id']
 
-vuln = vuln.merge(dmg['interpolation'].to_frame(), left_on='damage_bin_id',
-                  right_on="index")
-vuln.rename(index=str, columns={'interpolation': 'damageRatio'}, inplace=True)
+# Join
+vuln = pd.merge(vuln.reset_index(), dmg[['bin_from', 'bin_to']],
+                left_on="damage_bin_id",
+                right_index=True)
+vuln.rename(index=str, columns={'bin_to': 'UpperDamageRatio',
+                                'bin_from': 'LowerDamageRatio'},
+            inplace=True)
+vuln = vuln.assign(DamageRatio=0.5*(vuln.LowerDamageRatio +
+                                    vuln.UpperDamageRatio))
+
+# Read the intensity bins
 
 print("Generating plot...")
 fig, ax = plt.subplots(1, 1, facecolor='white')
 
 # Add the locations
-ax.scatter(vuln.intensity_bin_id, vuln.damageRatio,
-           s=1000*vuln.prob,
-           marker='o',
-           alpha=0.2, edgecolors=None)
+pts = ax.scatter(vuln.intensity_bin_id, vuln.DamageRatio,
+                 s=1+100*vuln.prob,
+                 c=vuln.exprob, cmap="RdYlGn",
+                 marker='o',
+                 alpha=0.5, edgecolors=None)
+
+cb = plt.colorbar(pts)
+cb.set_label("Exceedance Probability")
 
 # Formatting
+ax.set_facecolor("lightgray")
 ax.autoscale(tight=True)
 ax.grid()
 ax.set_xlabel('Intensity Bin ID')
 ax.set_ylabel('Damage Ratio')
+
+# Plot
+fig2, ax2 = plt.subplots(figsize=(9, 6))
+sns.heatmap(vuln.pivot('DamageRatio', 'intensity_bin_id', 'exprob'),
+            ax=ax2, cmap='Spectral_r', vmin=0.0, vmax=1.0, center=0.5)
+
+ax2.invert_yaxis()
+
 # Show the plot
 print('Pausing while plot is shown...')
 pylab.show(block=True)
